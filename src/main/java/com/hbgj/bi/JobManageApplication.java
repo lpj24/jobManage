c8e856119627c813428a6e88fbbbe04fa591109c
lpj24 <pengjuli24@gmail.com>
2017-05-08 13:56:29 +0800

    add druid

--- src/main/java/com/hbgj/bi/JobManageApplication.java
+++ src/main/java/com/hbgj/bi/JobManageApplication.java
@@ -1,14 +1,19 @@
 package com.hbgj.bi;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 //
 @MapperScan("com.hbgj.bi.mapper")
 @SpringBootApplication
 public class JobManageApplication {
+	private final static Logger logger = LoggerFactory.getLogger(JobManageApplication.class);  
 
 	public static void main(String[] args) {
+		logger.debug("server is running");
 		SpringApplication.run(JobManageApplication.class, args);
 
 	}
