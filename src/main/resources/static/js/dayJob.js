a072ea934e0d7754e14138c7e074a5dd0d9c60d5
lpj24 <pengjuli24@gmail.com>
2017-06-29 11:09:48 +0800

    add druid

--- src/main/resources/static/js/dayJob.js
+++ src/main/resources/static/js/dayJob.js
@@ -1,5 +1,36 @@
 $(function() {
 	
+	var utils = {
+		getSession: function(key) {
+			var returnResult = window.sessionStorage.getItem(key);
+			if(returnResult === null) {
+				return [];
+			}else{
+				return JSON.parse(returnResult);
+			}
+			
+		},
+		
+		setSession: function(key, value) {
+			window.sessionStorage.setItem(key, JSON.stringify(value));
+		},
+		appendSession: function(key, value) {
+			var returnResult = window.sessionStorage.getItem(key);
+			if(returnResult === null) {
+				window.sessionStorage.setItem(key, JSON.stringify(value));
+			}else{
+				returnResult = JSON.parse(returnResult);
+				returnResult = returnResult.concat(value);
+				window.sessionStorage.setItem(key, JSON.stringify(returnResult));
+			}
+
+		},
+		
+		clearSession: function() {
+			window.sessionStorage.clear();
+		}
+	}
+
 	$(".updateJob").text("更新前1天");
 
 	if($("#updateAllRecordNum").text() <= 0) {
@@ -8,7 +39,9 @@ $(function() {
 		$("#updateAllRecord").attr("disabled", false);
 	}
 	
-	$("#batchNum").text(JSON.parse(window.localStorage.getItem("batchList")).length);
+	var updateBatchList = utils.getSession("batchList");
+	$("#batchNum").text(updateBatchList.length);
+	
 	
 	$(".executeDay").change(function() {
 		var jobDays = $(this).val();
@@ -61,8 +94,9 @@ $(function() {
 	});
 	
 	//批量更新记录
-	var batchList = new Array()
+	
 	$(".batchUpdate").on("click", function() {
+		var batchList = new Array();
         rowIndex = this.parentNode.parentNode.rowIndex;
         var jobName = $(".table-hover tr:eq(" + rowIndex + ")" + " td:eq(0)" ).text();
         var jobPath = $(".table-hover tr:eq(" + rowIndex + ")" + " td:eq(1)" ).text();
@@ -81,37 +115,54 @@ $(function() {
         		jobDay: jobDay
         }
         batchList.push(batchObj);
-        window.localStorage.setItem("batchList", JSON.stringify(batchList));
-        $("#batchNum").text(batchList.length);
+        utils.appendSession("batchList", batchList)
+        $("#batchNum").text(utils.getSession("batchList").length);
 	});
 	
 	$("#batchQuery").on("click", function() {
 //		$("#allJobTable tr:not(:first)").html("");
 		var i = 0
-		var newBatchList = JSON.parse(window.localStorage.getItem("batchList"));
+		var newBatchList = utils.getSession("batchList");
 		$(".table-hover tr:eq(" + 0 + ")" + " th:eq(4)" ).text("");
 		$(".table-hover tr:eq(" + 0 + ")" + " th:eq(5)").text("");
 		$(".table-hover tr:eq(" + 0 + ")" + " th:eq(6)").text("");
 		for(i; i< newBatchList.length; i++) {
 			var j = i + 1;
-			
-			$(".table-hover tr:eq(" + j + ")" + " td:eq(0)" ).text(newBatchList[i].jobName);
-			$(".table-hover tr:eq(" + j + ")" + " td:eq(1)" ).text(newBatchList[i].jobPath);
-			$(".table-hover tr:eq(" + j + ")" + " td:eq(2)" ).text(newBatchList[i].jobDoc);
-			$(".table-hover tr:eq(" + j + ")" + " td:eq(3)" ).text(newBatchList[i].jobTable);
-			$(".table-hover tr:eq(" + j + ")" + " td:eq(4)" ).text(newBatchList[i].jobType);
-			$(".table-hover tr:eq(" + j + ")" + " td:eq(5)" ).text(newBatchList[i].renewable);
+			if($(".table-hover tr:eq(" + j + ")" + " td:eq(0)" ).html() === undefined) {
+				var td1 = "<td>" + newBatchList[i].jobName +"</td>"
+				var td2 = "<td>" + newBatchList[i].jobPath +"</td>"
+				var td3 = "<td>" + newBatchList[i].jobDoc +"</td>"
+				var td4 = "<td>" + newBatchList[i].jobTable +"</td>"
+				var td5 = "<td style='display:none;'>" + newBatchList[i].jobType +"</td>"
+				var td6 = "<td style='display:none;'>" + newBatchList[i].renewable +"</td>"
+				
+				
+				$(".table-hover tr:eq(" + j + ")" + " td:eq(6)" ).html("");
+				$(".table-hover tr:eq(" + j + ")" + " td:eq(7)").html("");
+				$(".table-hover tr:eq(" + j + ")" + " td:eq(8)").html("");
+				
+				$(".table-hover").append("<tr>" + td1 + td2 + td3 + td4 + td5 + td6 + "</tr>")
+			}else {
+				$(".table-hover tr:eq(" + j + ")" + " td:eq(0)" ).html(newBatchList[i].jobName);
+				$(".table-hover tr:eq(" + j + ")" + " td:eq(1)" ).html(newBatchList[i].jobPath);
+				$(".table-hover tr:eq(" + j + ")" + " td:eq(2)" ).html(newBatchList[i].jobDoc);
+				$(".table-hover tr:eq(" + j + ")" + " td:eq(3)" ).html(newBatchList[i].jobTable);
+				$(".table-hover tr:eq(" + j + ")" + " td:eq(4)" ).html(newBatchList[i].jobType);
+				$(".table-hover tr:eq(" + j + ")" + " td:eq(5)" ).html(newBatchList[i].renewable);
+				
+				$(".table-hover tr:eq(" + j + ")" + " td:eq(6)" ).html("");
+				$(".table-hover tr:eq(" + j + ")" + " td:eq(7)").html("");
+				$(".table-hover tr:eq(" + j + ")" + " td:eq(8)").html("");
+			}
 
-			
-			$(".table-hover tr:eq(" + j + ")" + " td:eq(6)" ).text("");
-			$(".table-hover tr:eq(" + j + ")" + " td:eq(7)").text("");
-			$(".table-hover tr:eq(" + j + ")" + " td:eq(8)").text("");
+
 		}
 		
 		$("#updateAllRecordNum").text($("#batchNum").text());
 		if($("#updateAllRecordNum").text() >= 1) {
 			$("#updateAllRecord").attr("disabled", false);
 		}
+
 		$("#allJobTable tr:gt(" + (i) + ")").remove();
 	})
 	
@@ -153,7 +204,7 @@ $(function() {
 			},
 			complete: function() {
 				$("#updateAllRecord").button('reset');
-				window.localStorage.clear();
+				utils.clearSession();
 			},
 			error: function() {
 				$("#updateAllRecord").button('reset');
@@ -161,4 +212,7 @@ $(function() {
 		})
 	})
 	
+	
 })
+
+
